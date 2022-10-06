d63dcdbb893be7c3976975f8925ca00c5a451a26
Dmitry Anikeev <47710990+anikeevgit@users.noreply.github.com>
2022-10-06 19:27:02 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ let numbInput = document.querySelector('#numb-input')
 let button = document.querySelector('#button')
 let button2 = document.querySelector('#button2')
 let button3 = document.querySelector('#button3')
+let firstP = document.querySelector('#first')
+let secondP = document.querySelector('#second')
 var result = ''
 var result2 = ''
 var result3 = ''
@@ -28,14 +30,17 @@ button2.addEventListener('click', function () {
   let str = result.split('')
   let str2 = result.match(search)
   let symb = symbInput.value
+  let c = 0
 
   for (let i = 0; i < str.length; i++) {
     for (let j = 0; j < str2.length; j++) {
       if (str[i] == str2[j]) {
         str[i] = symb
+        c++
       }
     }
   }
+  firstP.value = `Повторений ${symb} = ${c}`
   result2 = str.join('')
   resultInput2.value = result2
   resultInput3.value = result2
@@ -45,13 +50,16 @@ button3.addEventListener('click', function () {
   let str = result2.split('')
   let num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
   let numb = numbInput.value
+  let o = 0
   for (let i = 0; i < str.length; i++) {
     for (let j = 0; j < num.length; j++) {
       if (str[i] == num[j]) {
         str[i] = numb
+        o++
       }
     }
   }
+  secondP.value = `Повторений ${numb} = ${o}`
   result3 = str.join('')
   resultInput3.value = result3
 })
